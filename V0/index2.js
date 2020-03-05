9b1c05a84a577287ea154a5020b8fbb657af85f8
EastArctica <EastArctica@users.noreply.github.com>
2020-03-04 19:30:46 -0500

    Update index2.js

--- V0/index2.js
+++ V0/index2.js
@@ -8,29 +8,31 @@ corsServer.listen(3000, "localhost", function() {
 
 var UserName = "Bot"
 var UserCount = 1000;
-var PinCode = 0000000;
+var PinCode = 767620;
 
 const KahootAPI = require('kahoot-api')
 const Session = KahootAPI.Session
 const Adapters = KahootAPI.Adapters
 
-function AddBot(Pin, Username) {
-    var session = new Session(Pin, "http://localhost:3000/")
-    session.openSocket()
-        .then(socket => {
-            const player = new Adapters.Player(socket);
-            console.log('Attempting join with: ' + Username);
-            try {
-            player.join(Username)
-                .then(() => {
-                    console.log('Joined with: ' + Username);
-                });
-            } catch(e) {
-                console.log("Error Joining with: " + Username + " With error code: " + e)
-            }
-        });
+function AddBot(Pin, Username, CurrentI) {
+    setTimeout(function() {
+        var session = new Session(Pin, "http://localhost:3000/")
+        session.openSocket()
+            .then(socket => {
+                const player = new Adapters.Player(socket);
+                console.log('Attempting join with: ' + Username);
+                try {
+                player.join(Username)
+                    .then(() => {
+                        console.log('Joined with: ' + Username);
+                    });
+                } catch(e) {
+                    console.log("Error Joining with: " + Username + " With error code: " + e)
+                }
+            });
+    }, 50 * CurrentI);
 }
 
 for (i = 0; i < UserCount; i++) {
-    AddBot(PinCode, UserName + " - " + i)
+    AddBot(PinCode, UserName + " - " + i, i)
 }
